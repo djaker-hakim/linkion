989fabf9614bf3e0186e59ce9291915f64ab3b56
djaker hakim <hakiltheknight@gmail.com>
2025-10-04 07:41:38 +0100

    init commit

---
+++ src/LinkionServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Linkion;
+
+use Illuminate\Support\ServiceProvider;
+
+class LinkionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
